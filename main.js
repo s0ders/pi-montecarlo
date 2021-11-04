091df6b506687dbc00b71af601332b45d3938abf
s0ders <39492740+s0ders@users.noreply.github.com>
2021-11-04 21:56:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 const r = 1
 
-/**
- * Check if a point is inside the circle.
- */
 const landedIn = (x, y) => {
     return x**2 + y**2 < r**2
 }
 
-/**
- * Generates zero-centered float from -2 to 2.
- */
 const randomFloat = (from, to) => {
     return Math.random() < 0.5 ? (Math.random() - 0.5) * - (r*2) : (Math.random() - 0.5) * (r*2)
 }
 
-/**
- * Run a simulation by generating a number of points
- * with pseudo-random coordinates and approximate PI
- * using the number of points inside the circle of 
- * radius r divided by the total number of points.
- */
 const montecarloSimulation = (maxPoints) => {
     let pointsOut = 0
     let pointsIn = 0
@@ -34,8 +22,8 @@ const montecarloSimulation = (maxPoints) => {
     }
 
     const piEstimation = 4*pointsIn/maxPoints
-
-    console.log(`PI estimation: ${piEstimation}`)
+    
+    return piEstimation
 }
 
-montecarloSimulation(100000000)
+console.log(montecarloSimulation(10e6))
